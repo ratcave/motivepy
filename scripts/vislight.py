1c88095a62054b57f3500e2ed9b9efbc20f1fcd8
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-09-17 16:28:39 +0200

    vislight script is callable from everywhere (maybe the user who pulls it has to do python setup.py install first ?). One can choose ttp file via popup window and the updated file is then saved in same directory with same name but '_vislight' added

--- scripts/vislight.py
+++ scripts/vislight.py
@@ -22,15 +22,15 @@ for i in range(0, m.camera_count()):
         cam.set_settings(videotype=0, exposure=480, threshold=150, intensity=0) #check if 480 corresponds to these thousands described in motive
         cam.image_gain = 8  # 8 is the maximum image gain setting
         cam.set_filter_switch(False)
-
     else:
         cam.set_settings(0, cam.exposure, cam.threshold, cam.intensity)
 
 pathname_split=path.split(project_file)
 filename_split=path.splitext(pathname_split[1])
-saved_project_pathname=pathname_split[0]+filename_split[0]+'_vislight'+filename_split[1]
+saved_project_pathname=pathname_split[0]+'/'+filename_split[0]+'_vislight'+filename_split[1]
 m.save_project(saved_project_pathname)
 m.shutdown()
+
 
 
 
