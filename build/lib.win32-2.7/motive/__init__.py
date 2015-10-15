b44c5f6bc47a5d38fb54d81b23f97fa1144fbe13
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-10-15 20:17:27 +0200

    changed init files

--- build/lib.win32-2.7/motive/__init__.py
+++ build/lib.win32-2.7/motive/__init__.py
@@ -3,5 +3,6 @@ __author__ = 'nico'
 from native import *
 from camera import *
 from rigidbody import *
+from utils import viewer
 
 native._initialize()
--- motive/utils/__init__.py
+++ motive/utils/__init__.py
@@ -1,9 +1,3 @@
 __author__ = 'nico'
 
-import motive as m
-import Tkinter, tkFileDialog
-from numpy import array, linspace, vstack, zeros_like
-import time
-import itertools
-from pyqtgraph.Qt import QtCore, QtGui
-import pyqtgraph.opengl as gl
+import viewer
