d546a4e1c064e5d4ee77697e63bc75d7a1d08641
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-11-10 18:46:25 +0100

    btk should work fine

--- motive/utils/c3d_btk_writer.py
+++ motive/utils/c3d_btk_writer.py
@@ -2,21 +2,34 @@ __author__ = 'Nico'
 
 import btk
 
-def start_recording():
+def create_record_object():
     acq=btk.btkAcquisition()
     btk.btkAcquisition.Reset(acq)
     btk.btkAcquisition.Init(acq,len(m.get_frame_markers()),1)
 
-def record_frames_markers():
+    return acq
 
-    for i in len(m.get_frame_markers()):
+def record_frames_markers(acq):
+    frames=acq.GetPointFrameNumber()
+    markers=m.get_frame_markers()
+    record_points=acq.GetPoints()
+
+    if record_points.GetItemNumber()<len(markers):
+        btk.btkAcquisition.ResizePointNumber(acq, len(markers))
+
+    for i in xrange(len(markers)):
         point=acq.GetPoint(i)
-        frames=acq.GetPointFrameNumber()
-        x,y,z=m.get_frame_markers[i]
-        point.SetValue(frames,0,x)
-        point.SetValue(frames,1,y)
-        point.SetValue(frames,2,z)
+        point.SetDataSlice(frames-1, markers[i])
+    btk.btkAcquisition.ResizeFrameNumber(acq, frames+1)
 
+    return acq
 
+# def is_recording():
+
+def save_record_c3d(acq, file_name):
+    writer = btk.btkAcquisitionFileWriter()
+    writer.SetInput(acq)
+    writer.SetFilename(file_name)
+    writer.Update()
 
 
