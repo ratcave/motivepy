ec4ab59651cd5634d36ca5278bdf30ef2660f817
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-12-17 18:27:17 +0100

    video script almost finished, could not resolve the gui should return cam object error. But maybe next year ;)
    Former-commit-id: 9fe73a2a82b6e7d2de82e6bf0771a9d4e1a25f8c

--- scripts/video.py
+++ scripts/video.py
@@ -4,14 +4,53 @@ import time
 import numpy as np
 import argparse
 import motive as m
+import Tkinter, tkFileDialog
 
+def gui_load_project_file():
+    root = Tkinter.Tk()
+    root.withdraw()
+    project_file=(tkFileDialog.askopenfilename(title='Choose a project file to load: ', filetypes=[('motive projectfiles', '*.ttp')])).encode('ascii')
+    root.quit()
+    return project_file
 
 def get_cam(camera_name="91"):
     for cam in m.get_cams():
         if camera_name in cam.name:
             return cam
 
-def get_video_writer(video_file='video.avi'):
+def gui_camera_name():
+    #maybe try just returning the camera_name
+    root = Tkinter.Tk()
+
+    def choose_camera(camera_name):
+        #root.withdraw()
+        global cam
+        cam=get_cam(camera_name)
+        #root.quit()
+
+    frame=Tkinter.Frame(root)
+    frame.pack()
+
+    camera_button_11000=Tkinter.Button(frame, text="Camera Prime 13 #11000", command=lambda: choose_camera('11000'))
+    camera_button_11000.pack(side=Tkinter.LEFT)
+
+    camera_button_10187 = Tkinter.Button(frame, text="Camera Prime 17W #10187", command=lambda: choose_camera('10187'))
+    camera_button_10187.pack(side=Tkinter.LEFT)
+
+    camera_button_10189 = Tkinter.Button(frame, text="Camera Prime 17W #10189", command=lambda: choose_camera('10189'))
+    camera_button_10189.pack(side=Tkinter.LEFT)
+
+    camera_button_10910 = Tkinter.Button(frame, text="Camera Prime 13W #10910", command=lambda: choose_camera('10910'))
+    camera_button_10910.pack(side=Tkinter.LEFT)
+
+    Tkinter.mainloop()
+
+    return cam
+
+    root.quit() #doesnt work, the mainloop does not quit that way
+
+
+def get_video_writer(cam, video_file='video.avi'):
     """Show video of camera's frame buffer"""
 
     #Define the codec and create VideoWriter object
@@ -35,7 +74,7 @@ def get_video_writer(video_file='video.avi'):
                              )
     return writer
 
-def write_video(writer, record_time, save_video=True):
+def write_video(cam, writer, record_time, save_video=True):
     """
     If save_video=True, which is default, function shows video and writes it to file until
     end of record_time or Escape key or q is pressed.
@@ -46,7 +85,7 @@ def write_video(writer, record_time, save_video=True):
         k=cv2.waitKey(1)
         m.update()
         frame=cam.get_frame_buffer()
-        cv2.imshow('framerate={}'.format(cam.frame_rate),frame)
+        cv2.imshow('Live Video. Framerate={}Hz'.format(cam.frame_rate),frame)
         #frame=np.random.rand(480,640)
         #cv2.imshow('frame',frame)
 
@@ -69,10 +108,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="This is the motive camera video script. It can show and save the video data from any camera.",
                                      epilog="If no arguments are given, the script first opens a window to let you search for a project file to load. \n")
 
-    parser.add_argument('-p', action='store', dest='project_file', default=m.utils.backup_project_filename,
+    parser.add_argument('-p', action='store', dest='project_file', default='',
                         help='Name of the project file to load.')
 
-    parser.add_argument('-c', action='store', dest='camera_name', default='91',
+    parser.add_argument('-c', action='store', dest='camera_name', default='',
                         help='Name of the camera from which to get the video.')
 
     parser.add_argument('-f', action='store', dest='video_filename', default='video.avi',
@@ -87,14 +126,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-    m.load_project(args.project_file)
-    cam=get_cam(args.camera_name)
+    m.load_project(args.project_file) if args.project_file else m.load_project(gui_load_project_file())
+
+    if args.camera_name:
+        cam=get_cam(args.camera_name)
+    else:
+        cam=gui_camera_name()
 
     #Get Video Writer
     if args.save_video:
-        writer=get_video_writer(args.video_filename)
+        writer=get_video_writer(cam, args.video_filename)
 
-    write_video(writer, args.record_time, args.save_video)
+    write_video(cam, writer, args.record_time, args.save_video) #again giving cam as argument, since writer object
+                                                                #has no property to extract frame rate and frame_size from
     if args.save_video:
         writer.release()
     cv2.destroyAllWindows()
