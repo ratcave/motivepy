f4d2c8f97ae81d313f730fc59591fe28f224e572
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2018-06-11 17:05:27 +0200

    changed paths to be os-neutral.

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@ from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import numpy
 import os
+from os import path
 from warnings import warn
 
 
-MOTIVE_INC_DIR="C:\\Program Files\\OptiTrack\\Motive\\inc"
-MOTIVE_LIB_DIR="C:\\Program Files\\OptiTrack\\Motive\\lib"
+MOTIVE_INC_DIR = path.join("C:", "Program Files", "OptiTrack", "Motive", "inc")
+MOTIVE_LIB_DIR = path.join("C:", "Program Files", "OptiTrack", "Motive", "lib")
 MOTIVE_LINK_ARG="/DEFAULTLIB:NPTrackingToolsx64"
 
 #TODO: Link to correct path automatically
 if not os.path.exists(MOTIVE_INC_DIR):
-    warn("\n \n Can't find path {0}. Please use Motive 64bit \n \n".format(MOTIVE_INC_DIR))
+    warn("Can't find path {0}. Continuing installation, but please ensure Motive 64bit files are there before running.".format(MOTIVE_INC_DIR))
 
 
 native = Extension(
     'motive.native',
-    sources=["src\\native.pyx"],
+    sources=[path.join("src", "native.pyx")],
     include_dirs=[MOTIVE_INC_DIR, "src"],
     library_dirs=[MOTIVE_LIB_DIR ],
     extra_link_args=[MOTIVE_LINK_ARG],
@@ -25,7 +26,7 @@ native = Extension(
 
 camera = Extension(
     'motive.camera',
-    sources=["src\\camera.pyx"],
+    sources=[path.join("src", "camera.pyx")],
     include_dirs=[MOTIVE_INC_DIR, "src", numpy.get_include()],
     library_dirs=[MOTIVE_LIB_DIR ],
     extra_link_args=[MOTIVE_LINK_ARG],
@@ -34,7 +35,7 @@ camera = Extension(
 
 rigidbody = Extension(
     'motive.rigidbody',
-    sources=["src\\rigidbody.pyx" ],
+    sources=[path.join("src", "rigidbody.pyx")],
     include_dirs=[MOTIVE_INC_DIR, "src"],
     library_dirs=[MOTIVE_LIB_DIR ],
     extra_link_args=[MOTIVE_LINK_ARG],
