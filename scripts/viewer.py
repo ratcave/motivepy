84656363b4e0dbe5f53aa580a2a4a020dac6a13a
Issahar <issahar@onsight-med.com>
2020-06-15 10:49:46 +0300

    more changes from project to profile

--- scripts/viewer.py
+++ scripts/viewer.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="This is the motive viewer script. It displays the unidentified markers and the rigid bodies in real time.",
                                      epilog="If no arguments are given, the script first opens a window to let you search for a project file to load. \n")
 
-    parser.add_argument('-l', action='store_true', dest='last_project', default=False,
-                        help='If this flag is set, the last project file is loaded.')
+    parser.add_argument('-l', action='store_true', dest='last_profile', default=False,
+                        help='If this flag is set, the last profile file is loaded.')
 
-    parser.add_argument('-p', action='store', dest='project_filename', default='',
-                        help='Name of the project file to load.')
+    parser.add_argument('-p', action='store', dest='profile_filename', default='',
+                        help='Name of the profile file to load.')
 
     parser.add_argument('-love', action='store', dest='love', default='',
                         help='Plot a heart instead.')
@@ -28,24 +28,24 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Get Project
-    if args.last_project:
-        if args.project_filename is True:
-            raise Warning("Load last project file OR name a project file to load.")
-        project_file=utils.backup_profile_filename
+    if args.last_profile:
+        if args.profile_filename is True:
+            raise Warning("Load last profile file OR name a profile file to load.")
+        profile_file=utils.backup_profile_filename
 
-    elif args.project_filename:
-        if args.last_project is True:
-            raise Warning("Load last project file OR name a project file to load.")
-        project_file=args.project_filename
+    elif args.profile_filename:
+        if args.last_profile is True:
+            raise Warning("Load last profile file OR name a profile file to load.")
+        profile_file=args.profile_filename
 
     else:
         root = Tkinter.Tk()
         root.withdraw()
-        project_file_u=tkFileDialog.askopenfilename(title='Choose a profile file to load: ', filetypes=[('motive profilefiles', '*.motive')])
-        project_file = project_file_u.encode("ascii")
+        profile_file_u=tkFileDialog.askopenfilename(title='Choose a profile file to load: ', filetypes=[('motive profilefiles', '*.motive')])
+        profile_file = profile_file_u.encode("ascii")
 
     # Load Motive Project
-    motive.load_project(project_file)
+    motive.load_profile(profile_file)
 
     if args.love:
         import matplotlib.pyplot as plt
